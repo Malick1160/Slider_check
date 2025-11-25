4f6b66577dc802e6254f4334e83164b8d161d6ff
salemmenadia57@gmail.com <salemmenadia57@gmail.com>
2025-11-25 20:09:50 +0100

    Added min and max vol value

--- source/Slider_check.c
+++ source/Slider_check.c
@@ -7,8 +7,8 @@
 int main(int argc, char * argv[])
 {
 	
-	unsigned char i2c_3dslider, i2c_volslider;
-	Result res_3d, res_vol, mcu_init;
+	unsigned char i2c_3dslider, i2c_volslider, volmax,volmin;
+	Result res_3d, res_vol, mcu_init, res_volmax, res_volmin;
 
 	gfxInitDefault(); //init services 
 	mcu_init = mcuHwcInit();
@@ -27,20 +27,36 @@ int main(int argc, char * argv[])
 		res_vol = MCUHWC_ReadRegister(0x27, &i2c_volslider, 1);//get raw volume slider position from mcu reg
 		res_3d = MCUHWC_ReadRegister(0x8, &i2c_3dslider, 1);//get raw 3d slider position from mcu reg
 
+		res_volmin = MCUHWC_ReadRegister(0x58, &volmin, 1);//get minimum volume from mcu reg
+		res_volmax = MCUHWC_ReadRegister(0x59, &volmax, 1);//get maxiomum volume from mcu reg
 
-		if (R_FAILED(res_vol)) {//if it somehow fails to read volume slider from MCU, very bad if it happens
+		if (R_FAILED(res_vol)) {//if it somehow fails to read data from MCU, very bad if it happens
 			printf("Failed to read volume slider data!\n\n(How did this happen?)\n\nCode: 0x%08lX", res_vol);
-			sleep(20);
+			sleep(15);
 			break;
 		}
 
-		if (R_FAILED(res_3d)) {//if it somehow fails to read 3d slider from MCU, very bad if it happens
+		if (R_FAILED(res_3d)) {
 			printf("Failed to read 3D slider data!\n\n(How did this happen?)\n\nCode: 0x%08lX", res_3d);
-			sleep(20);
+			sleep(15);
 			break;
 		}
 
-		printf("Welcome to Slider_check! Hold START to quit.\n\ni2c Volume(Raw Value): %3u %02X \e[K\r\n\n" "i2c 3d(Raw Value): %3u %02X \e[K\r\e[5A", i2c_volslider, i2c_volslider, i2c_3dslider, i2c_3dslider);
+		if (R_FAILED(res_volmin)) {
+			printf("Failed to read minimum volume!\n\n(How did this happen?)\n\nCode: 0x%08lX", res_volmin);
+			sleep(15);
+			break;
+		}
+
+		if (R_FAILED(res_volmax)) {
+			printf("Failed to read maximum volume!\n\n(How did this happen?)\n\nCode: 0x%08lX", res_volmin);
+			sleep(15);
+			break;
+		}
+
+		printf("Welcome to Slider_check! Hold START to quit.\n\n" "Max volume: %3u %02X\e[K\r\n\n" "Min volume: %3u %02X\e[K\r\n\n"
+		"Volume slider position: %3u %02X \e[K\r\n\n" "3d slider position: %3u %02X \e[K\r\e[9A",
+		volmax, volmax, volmin, volmin, i2c_volslider, i2c_volslider, i2c_3dslider, i2c_3dslider);
 		
 		hidScanInput();//quit by holding start
 
