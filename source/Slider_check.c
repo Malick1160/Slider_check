6354aa2c3175458878b2601e63749eea6a1f1647
salemmenadia57@gmail.com <salemmenadia57@gmail.com>
2026-01-06 03:53:26 +0100

    Added headphone check

--- source/Slider_check.c
+++ source/Slider_check.c
@@ -7,22 +7,39 @@
 int main(int argc, char * argv[])
 {
 	
-	unsigned char i2c_3dslider, i2c_volslider, volmax,volmin;
-	Result res_3d, res_vol, mcu_init, res_volmax, res_volmin;
+	unsigned char i2c_3dslider, i2c_volslider, volmax, volmin;
+	Result res_3d, res_vol, mcu_init, dsp_init, res_volmax, res_volmin, res_headphone;
+	bool hasHeadphones;
 
 	gfxInitDefault(); //init services 
 	mcu_init = mcuHwcInit();
-
+	dsp_init = dspInit();
 	consoleInit(GFX_TOP, NULL);
+
+	if (R_FAILED(mcu_init)) {//mcuHcInit can fail due to old luma or *hax setup and in that case we immediately stop the program to prevent unintended effects
+		printf("mcuHwcInit Failed! (outdated setup?)\n\nCode: 0x%08lX", mcu_init);
+		sleep(15);
+		// Exit services
+		mcuHwcExit();
+		gfxExit();
+		dspExit();
+		return -1;
+	}
+
+	if (R_FAILED(dsp_init)) {//check if dspInit fails
+		printf("dspInit Failed! (How did this happen?)\n\nCode: 0x%08lX", dsp_init);
+		sleep(15);
+		// Exit services
+		mcuHwcExit();
+		gfxExit();
+		dspExit();
+		return -1;
+	}
 
 	while (aptMainLoop())
 	{
-		
-		if (R_FAILED(mcu_init)) {//mcuHcInit can fail due to old luma or *hax setup and in that case we immediately stop the program to prevent unintended effects
-			printf("mcuHwcInit Failed! (outdated setup?)");
-			sleep(20);
-			break;
-		}
+
+		res_headphone = DSP_GetHeadphoneStatus(&hasHeadphones);//check if headphone jack is inserted
 
 		res_vol = MCUHWC_ReadRegister(0x27, &i2c_volslider, 1);//get raw volume slider position from mcu reg
 		res_3d = MCUHWC_ReadRegister(0x8, &i2c_3dslider, 1);//get raw 3d slider position from mcu reg
@@ -30,7 +47,13 @@ int main(int argc, char * argv[])
 		res_volmin = MCUHWC_ReadRegister(0x58, &volmin, 1);//get minimum volume from mcu reg
 		res_volmax = MCUHWC_ReadRegister(0x59, &volmax, 1);//get maxiomum volume from mcu reg
 
-		if (R_FAILED(res_vol)) {//if it somehow fails to read data from MCU, very bad if it happens
+		if (R_FAILED(res_headphone)) {//check if it fails to detect headphone data
+			printf("Failed to read headphone jack data!\n\n(How did this happen?)\n\nCode: 0x%08lX", res_vol);
+			sleep(15);
+			break;
+		}
+
+		if (R_FAILED(res_vol)) {//the following 4 checks all involve the MCU, so if one of them fails it's very bad
 			printf("Failed to read volume slider data!\n\n(How did this happen?)\n\nCode: 0x%08lX", res_vol);
 			sleep(15);
 			break;
@@ -55,8 +78,8 @@ int main(int argc, char * argv[])
 		}
 
 		printf("Welcome to Slider_check! Hold START to quit.\n\n" "Max volume: %3u %02X\e[K\r\n\n" "Min volume: %3u %02X\e[K\r\n\n"
-		"Volume slider position: %3u %02X \e[K\r\n\n" "3d slider position: %3u %02X \e[K\r\e[9A",
-		volmax, volmax, volmin, volmin, i2c_volslider, i2c_volslider, i2c_3dslider, i2c_3dslider);
+		"Volume slider position: %3u %02X \e[K\r\n\n" "3d slider position: %3u %02X \e[K\r\n\n" "Headphones: %s\e[K\r\e[11A",
+		volmax, volmax, volmin, volmin, i2c_volslider, i2c_volslider, i2c_3dslider, i2c_3dslider, hasHeadphones ? "TRUE" : "FALSE");
 		
 		hidScanInput();//quit by holding start
 
@@ -73,5 +96,6 @@ int main(int argc, char * argv[])
 	// Exit services
 	mcuHwcExit();
 	gfxExit();
+	dspExit();
 	return 0;
 }
